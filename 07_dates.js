2f5d1be58bc18792bfc4af49752c9b6976d6eb06
Rushi-shinde <shinderushi327@gmail.com>
2024-03-30 21:28:55 +0530

    Dates and Timestamp

---
+++ 07_dates.js
@@ -0,0 +1,47 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toTimeString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate2 = new Date(2023, 0, 23)
+// // months started with 0(zero)
+
+
+// console.log(myCreatedDate2.toLocaleDateString())
+
+
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate2 = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+// console.log(myCreatedDate2.toLocaleString())
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(myCreatedDate2.getTime())
+
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+// console.log(newDate.getTime())
+// console.log(newDate.getDate())
+// console.log(newDate.getMonth())
+// console.log(newDate.getFullYear())
+// console.log(newDate.getHours())
+// console.log(newDate.getSeconds())
+// console.log(newDate.getMinutes())
+
+
+// newDate.toLocaleString('default', {
+//     weekday : "long",
+// })
+
+// console.log(newDate)
