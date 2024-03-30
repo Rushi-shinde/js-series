4173ccdbc8cc36d6a281d37c7b9f9af668e589dd
Rushi-shinde <shinderushi327@gmail.com>
2024-03-30 21:05:40 +0530

    Math and Num

---
+++ 06_numsAndMaths.js
@@ -0,0 +1,43 @@
+// const score = 400
+// console.log(score)
+
+
+// const balance = new Number(400)
+// console.log(balance)
+// console.log(typeof balance)
+// console.log(balance.toString()) 
+// console.log(balance.toFixed(2)) 
+
+
+// const otherNumber = 1233.123456
+// console.log(otherNumber.toPrecision(4))
+
+// const abc = 1000000
+// console.log(abc.toLocaleString('en-IN'))
+
+// const maxvalue = Number.MAX_VALUE
+// const minvalue = Number.MIN_VALUE
+
+// console.log(maxvalue)
+// console.log(minvalue)
+
+
+//+++++++++++++++++ Maths +++++++++++++++++++++++++++++++++//
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.4))
+// console.log(Math.floor(4.4))
+
+// console.log(Math.sqrt(25))
+// console.log(Math.max(3,4,2,2,1))
+// console.log(Math.min(3,4,2,2,1))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
